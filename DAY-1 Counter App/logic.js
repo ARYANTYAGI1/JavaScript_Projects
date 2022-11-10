68ab4f9fc073936a7998a1b2db1fcbc5319f2bab
Aryan Tyagi <85342115+ARYANTYAGI1@users.noreply.github.com>
2022-11-10 18:58:29 +0530

    Add files via upload

---
+++ DAY-1 Counter App/logic.js
@@ -0,0 +1,16 @@
+var count=0;
+
+function handleUp(){
+    count=count+1;
+    document.getElementById('result').innerHTML=count;
+}
+function handleDown(){
+    if(count>0){
+    count=count-1;
+    }
+    document.getElementById('result').innerHTML=count;
+}
+function handleReset(){
+    count=0;
+    document.getElementById('result').innerHTML=count;
+}
